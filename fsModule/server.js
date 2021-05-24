6cd14a607d057f12721c08cbb6f303a4d1a97181
Sajjad Alam <sajjadalam3577@gmail.com>
2021-05-24 11:16:37 +0530

    fs Module

--- fsModule/server.js
+++ fsModule/server.js
@@ -1,13 +1,62 @@
-const http = require("http");
-const fs = require("fs");
+// // Reading file in fs Module
+// const fs = require("fs");
 
-let server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.write("<h1>Hello to fs Module </h1>");
-  res.end();
-});
+// Buffer is additional datatype in nodejs
+// Buffers are mainly used to store binary data while reading from the file or reciving packets from internet
+// let buf_data = fs.readFileSync("./sajjad.txt");
+// console.log(buf_data);
 
-// assign port
-server.listen(8080, () => {
-  console.log("server is listing to the port no 8080");
-});
+// // for async you must be use callback
+// fs.readFile("./sajjad.txt", (err, data) => {
+//   if (err) throw err;
+//   else {
+//     console.log(data);
+//   }
+// });
+
+// Writing Files in fs Module
+
+// const fs = require("fs");
+
+// // fs.writeFileSync("./alam.txt", "Hello This is alam page");
+
+// // async
+// fs.writeFile("./alam.txt", "", (err) => {
+//   if (err) throw err;
+//   console.log("file created successfully");
+// });
+
+// Appending of Data
+// let data = "Hello sajjad lorem ipsumm  ksd hsh";
+// require("fs").appendFile("./sajjad.txt", data, (err) => {
+//   if (err) throw err;
+//   console.log("Data appended successfuly");
+// });
+// // sync
+// require("fs").appendFileSync("./alam.txt", "hi");
+// require("fs").appendFileSync("./alam.txt", " hi sajjad ");
+
+//
+// sync
+// require("fs").truncateSync("./sajjad.txt", 7);
+// // async
+// require("fs").truncate("./alam.txt", 1, (err) => {
+//   if (err) throw err;
+//   console.log("Contentent deleted successfully");
+// });
+
+// delteting file by using fs module
+
+// require("fs").unlink("./alam.txt", (err) => {
+//   if (err) throw err;
+//   console.log("File Deleted successfully");
+// });
+
+// require("fs").unlinkSync("./sajjad.txt");
+
+// openfile
+
+// require("fs").open("./sajjad.txt", "w", (err) => {
+//   if (err) throw err;
+//   console.log("file created successfully");
+// });
